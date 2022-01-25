26b3a0612314b07bcfa19482c18a6bd892a4a411
VinyProgramador <estudosviny@gmail.com>
2022-01-25 19:18:34 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import React from "react";
+import gem from './assets/gem.svg'
+import logo from './img/logo.png'
+import './styles.css'
 
 function App() {
+  const [number, setConta] = useState("")
+  const [password, setAgencia] = useState("")
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <h3 className="login-form-title">CrystalBank</h3>
+
+
+            <span className="login-form-title">
+              <img src={logo} alt="CB" />
+            </span>
+
+            <div className="wrap-input">
+              <input className={number !== "" ? 'has-val input' : 'input'} type="number" value={number} onChange={e => setConta(e.target.value)} required />
+              <span className="focus-input" data-placeholder="Conta"></span>
+            </div>
+
+            <div className="wrap-input">
+              <input className={password !== "" ? 'has-val input' : 'input'} type="password" value={password} onChange={e => setAgencia(e.target.value)} required />
+              <span className="focus-input" data-placeholder="Agencia"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+            <input class="btn" type="submit" value="Entrar"/>
+            </div>
+
+            <div className="text-center">
+              <span className="txt1"> Não Possui Conta?  </span>
+              <a className="txt2" href="#">Criar</a>
+            </div>
+
+          </form>
+        </div>
+
+      </div>
     </div>
   );
 }
